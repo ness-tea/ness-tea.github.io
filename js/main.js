bf88aa58fbac89758d21652537acc89675e1da44
nessa-art <nessenceart@gmail.com>
2019-04-27 01:00:03 -0400

    Added slick carousel to project section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
   $('.project-arr').slick({
-    setting-name: setting-value
+    centerMode: true,
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 100,
+        settings: {
+          dots: true,
+          arrows: true,
+          centerMode: true,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          dots: true,
+          arrows: true,
+          centerMode: true,
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 });
