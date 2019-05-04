5dca2399baad7e119be7c34e8c970d22272777e3
nessa-art <nessenceart@gmail.com>
2019-05-04 15:11:28 -0400

    added slick carousel effect to art section

--- js/main.js
+++ js/main.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
     slidesToShow: 3,
     slidesToScroll: 1
   });
+
+  $('.art-arr').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1700
+  });
 });
