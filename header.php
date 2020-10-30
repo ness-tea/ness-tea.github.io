14bacc766bc6894e59bbfd43d3d7879f3f0ea59a
Vanessa Truong <v.truong9710@outlook.com>
2020-10-30 04:06:31 -0400

    Add redesign.

---
+++ header.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Vanessa Truong | Art ⬩ Software</title>
+
+    <!--- Google fonts --->
+    <link href="https://fonts.googleapis.com/css2?family=Handlee&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Rancho&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Pacifico" rel="stylesheet">
+
+    <!--- CSS Styling --->
+    <link href="css/main_new.css" type="text/css" rel="stylesheet">
+
+    <link rel="icon" href="assets/Logos/favicon.png">
+</head>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<?php 
+    include("header.php");
+    include("nav.php");
+?>
+
+<head>
+    <meta charset="utf-8">
+    <title>Vanessa Truong | Art ⬩ Software</title>
+
+    <!--- Google fonts --->
+    <link href="https://fonts.googleapis.com/css2?family=Handlee&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Rancho&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Pacifico" rel="stylesheet">
+
+    <!--- CSS Styling --->
+    <link href="css/main_new.css" type="text/css" rel="stylesheet">
+
+    <link rel="icon" href="assets/Logos/favicon.png">
+</head>
+
+<body>
+    <h2 class="adjust-margin-bottom" id="h2-hero">Hi there! I'm</h2>
+    <h1>Vanessa </h1>
+    <h1 id="h1-style2">Truong</h1>
+
+    <br></br><br></br><br></br>
+    <h3>... more coming soon!</h3>
+</body>
+
+<?php 
+    include("footer.php");
+?>
