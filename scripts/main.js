d337486816f4e5b41f1877d4edbe9e9f189a2b8e
Vanessa Truong <v.truong9710@outlook.com>
2021-05-13 01:50:25 -0400

    Update hero.

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
 // Globals
-var urlindex = 0;
-var urls = ["assets/img/backgrounds/homepage2.jpg",
-            "assets/img/backgrounds/homepage3.jpg"];
+// var urlindex = 0;
+// var urls = ["assets/graphics/hero1.png",
+//             "assets/graphics/hero1.png"];
 
-// Set background every 1000 ms for GIF effect. 
-function setBackgroundImage() {
-    setInterval(changeBackground, 1000);
-}
+// // Set background every 1000 ms for GIF effect. 
+// function setBackgroundImage() {
+//     setInterval(changeBackground, 1000);
+// }
 
-// Changes background.
-function changeBackground() {
-    if (urlindex == urls.length)
-    {
-        urlindex = 0;
-    }
-    document.getElementById('hero').style.backgroundImage = 'url(' + urls[urlindex] + ')';
-    urlindex += 1;          
-}
+// // Changes background.
+// function changeBackground() {
+//     if (urlindex == urls.length)
+//     {
+//         urlindex = 0;
+//     }
+//     document.getElementById('hero').style.backgroundImage = 'url(' + urls[urlindex] + ')';
+//     urlindex += 1;          
+// }
